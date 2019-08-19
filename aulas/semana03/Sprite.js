1f6b5dee9e3e7173d155c0e24d167d2c57cdbd3e
ICE <ice@lab04m47.nrc.ice.ufjf.br>
2019-08-19 19:47:36 -0300

    Adiciona prototipo e função para desenhar

--- aulas/semana03/Sprite.js
+++ aulas/semana03/Sprite.js
@@ -3,6 +3,16 @@ function Sprite() {
     this.y = 0;
     this.vx = 0;
     this.vy = 0;
-    this.w = 30;
-    this.h = 30;
+    this.w = 20;
+    this.h = 20;
+}
+
+Sprite.prototype = new Sprite();
+Sprite.constructor = Sprite;
+
+Sprite.prototype.desenhar = function(ctx) {
+    ctx.fillStyle = "lightblue";
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(this.x, this.y, this.w, this.h);
 }
