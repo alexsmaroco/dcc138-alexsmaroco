7c68fc807a8cba6e1cca8a0ec8d20b6db8718179
ICE <ice@lab04m47.nrc.ice.ufjf.br>
2019-08-22 19:37:10 -0300

    Add array de npcs e controle por teclas

--- aulas/semana03/Sprite.js
+++ aulas/semana03/Sprite.js
@@ -1,11 +1,12 @@
-function Sprite(params) {
+function Sprite(params = {}) {
     var {
         x = 0,
         y = 0,
         vx = 0,
         vy = 0,
         w = 20,
-        h = 20
+        h = 20,
+        color = "blue"
     } = params;
     this.x = x;
     this.y = y;
@@ -13,16 +14,17 @@ function Sprite(params) {
     this.vy = vy;
     this.w = w;
     this.h = h;
+    this.color = color;
 }
 
 Sprite.prototype = new Sprite({});
 Sprite.constructor = Sprite;
 
 Sprite.prototype.desenhar = function(ctx) {
-    ctx.fillStyle = "lightblue";
+    ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.w, this.h);
     ctx.strokeStyle = "black";
-    ctx.strokeRect(this.x, this.y, this.w, this.h);
+    //  ctx.strokeRect(this.x, this.y, this.w, this.h);
 }
 
 Sprite.prototype.mover = function(dt) {
@@ -30,9 +32,9 @@ Sprite.prototype.mover = function(dt) {
     this.y = this.y + this.vy * dt;
 }
 
-Sprite.prototype.perseguir = function(alvo) {
-    this.vx = 50*Math.sign(alvo.x - this.x);
-    this.vy = 50*Math.sign(alvo.y - this.y);
+Sprite.prototype.perseguir = function(opcoes) {
+    this.vx = 40*Math.sign(opcoes.alvo.x - this.x);
+    this.vy = 40*Math.sign(opcoes.alvo.y - this.y);
 }
 
 Sprite.prototype.colidiuCom = function(alvo) {
@@ -41,4 +43,11 @@ Sprite.prototype.colidiuCom = function(alvo) {
     if(alvo.y + alvo.h < this.y) return false;
     if(alvo.y > this.y + this.h) return false;
     return true;
+}
+
+Sprite.prototype.controlePorTeclas = function(opcoes) {
+    if(opcoes.teclas.esquerda) { this.vx = -50; }
+    if(opcoes.teclas.direita) { this.vx = 50; }
+    if(opcoes.teclas.cima) { this.vy = -50; }
+    if(opcoes.teclas.baixo) { this.vy = 50; }
 }
