28537af9bebf62fe79898759c99c0ef6f89599ab
ICE <ice@lab04m47.nrc.ice.ufjf.br>
2019-08-22 20:21:34 -0300

    Adiciona imunidade temporária ao ser atingido

--- aulas/semana03/Sprite.js
+++ aulas/semana03/Sprite.js
@@ -6,7 +6,8 @@ function Sprite(params = {}) {
         vy = 0,
         w = 20,
         h = 20,
-        color = "blue"
+        color = "blue",
+        imune = 0
     } = params;
     this.x = x;
     this.y = y;
@@ -15,26 +16,31 @@ function Sprite(params = {}) {
     this.w = w;
     this.h = h;
     this.color = color;
+    this.imune = imune;
 }
 
 Sprite.prototype = new Sprite({});
 Sprite.constructor = Sprite;
 
 Sprite.prototype.desenhar = function(ctx) {
+    if(this.imune > 0) {
+        ctx.globalAlpha = 0.5*(Math.cos(60*this.imune));
+    }
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.w, this.h);
-    ctx.strokeStyle = "black";
-    //  ctx.strokeRect(this.x, this.y, this.w, this.h);
+    ctx.globalAlpha = 1.0;
 }
 
 Sprite.prototype.mover = function(dt) {
+    if(this.imune > 0)
+        this.imune-=dt;
     this.x = this.x + this.vx * dt;
     this.y = this.y + this.vy * dt;
 }
 
 Sprite.prototype.perseguir = function(opcoes) {
-    this.vx = 40*Math.sign(opcoes.alvo.x - this.x);
-    this.vy = 40*Math.sign(opcoes.alvo.y - this.y);
+    this.vx = 30*Math.sign(opcoes.alvo.x - this.x);
+    this.vy = 30*Math.sign(opcoes.alvo.y - this.y);
 }
 
 Sprite.prototype.colidiuCom = function(alvo) {
@@ -46,8 +52,11 @@ Sprite.prototype.colidiuCom = function(alvo) {
 }
 
 Sprite.prototype.controlePorTeclas = function(opcoes) {
-    if(opcoes.teclas.esquerda) { this.vx = -50; }
-    else if(opcoes.teclas.direita) { this.vx = 50; } else { this.vx = 0; }
-    if(opcoes.teclas.cima) { this.vy = -50; }
-    else if(opcoes.teclas.baixo) { this.vy = 50; } else { this.vy = 0; }
+    if(opcoes.teclas.esquerda) { this.vx = -70; }
+    else if(opcoes.teclas.direita) { this.vx = 70; } else { this.vx = 0; }
+    // if(!opcoes.teclas.esquerda && !opcoes.teclas.direita) {this.vx=0;}
+    if(opcoes.teclas.cima) { this.vy = -70; }
+    else if(opcoes.teclas.baixo) { this.vy = 70; } else { this.vy = 0; }
+    // if(!opcoes.teclas.cima && !opcoes.teclas.baixo) {this.vy=0;}
 }
+
