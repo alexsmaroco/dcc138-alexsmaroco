7666ee3651840953dc2be5bba81ed739f46bf7a9
alexsmaroco <ice@lab04m47.nrc.ice.ufjf.br>
2019-08-26 20:15:41 -0300

    Refatora código da semana 3

---
+++ aulas/semana04/Scene.js
@@ -0,0 +1,33 @@
+function Scene(params) {
+    var exm = {
+        sprites: [],
+        ctx: null,
+        w: 400,
+        h: 400,
+    }
+    Object.assign(this, exm, params);
+}
+
+Scene.prototype = new Scene();
+Scene.prototype.constructor = Scene;
+
+Scene.prototype.desenhar = function() {
+    for(var i = 0; i < this.sprites.length; i++) {
+        this.sprites[i].desenhar(this.ctx);
+    }
+}
+
+Scene.prototype.mover = function(dt) {
+    for(var i = 0; i < this.sprites.length; i++) {
+        this.sprites[i].mover(dt);
+    }
+}
+
+Scene.prototype.limpar = function() {
+    this.ctx.fillStyle = "grey";
+    this.ctx.fillRect(0,0,this.w, this.h);
+}
+
+Scene.prototype.adicionar = function(sprite) {
+    this.sprites.push(sprite);
+}
