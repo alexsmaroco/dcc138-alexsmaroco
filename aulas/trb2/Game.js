e25917e71968ad45e6abeb5d54cfb95d131f4413
alexsmaroco <ice@lab04m47.nrc.ice.ufjf.br>
2019-10-17 20:37:04 -0300

    Adiciona explosões em cadeia

--- aulas/trb2/Game.js
+++ aulas/trb2/Game.js
@@ -17,7 +17,7 @@ function init(){
   images = new ImageLoader();
   images.load("pc","assets/pc.png");
   map = new Map(Math.floor(canvas.height/40), Math.floor(canvas.width/40));
-  map.images = images;
+  //map.images = images;
   map.setCells([
     [1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,0,0,2,2,2,2,2,2,2,2,2,1],
@@ -33,8 +33,7 @@ function init(){
     [1,2,2,2,2,2,2,2,2,2,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1],
   ]);
-  map.cooldownPowerup = 5;
-  // spawna powerups no map !! problema: pode ter mais de 1 no mesmo lugar
+  //map.cooldownPowerup = 5;
   map.spawnPowerupFixo(10);
   pc1 = new Sprite();
   pc1.id = "1";
@@ -42,15 +41,14 @@ function init(){
   pc1.y = 60;
   pc1.vidas = 3;
   pc1.imunidade = 1;
-  pc1.images = images;
   pc1.imgkey = "pc";
+
   pc2 = new Sprite();
   pc2.id = "2";
   pc2.x = 460;
   pc2.y = 380+80;
   pc2.vidas = 3;
   pc2.imunidade = 1;
-  pc2.images = images;
   pc2.imgkey = "pc";
   initControls();
   requestAnimationFrame(passo);
@@ -81,12 +79,11 @@ function passo(t){
 		//map.spawnPowerup(dt);
 		pc1.mover(map, dt);
 		pc2.mover(map, dt);
-		
 	}
 
-	map.desenhar(ctx);
-	pc1.desenhar(ctx);
-	pc2.desenhar(ctx);
+	map.desenhar(ctx, images);
+	pc1.desenhar(ctx, images);
+	pc2.desenhar(ctx, images);
     desenhaInfo(ctx);
 	anterior = t;
 }
@@ -115,76 +112,91 @@ function explodir(bomb, map) {
 	}
 	// verifica os arredores
 	for(var i = 1; i <= bomb.power; i++) {
-		if(gy-i >= 0) {
+		if(gy-i >= 0 && destruir1) {
+			switch(map.cells[gy-i][gx].tipo) {
+				case "paredeInd":
+					destruir1 = false;
+					break;
+				case "paredeDest":
+					map.cells[gy-i][gx].tipo = "vazio";
+					destruir1 = false;
+					break;
+				case "bomba":
+					map.cells[gy-i][gx].tipo = "vazio";
+					chainReaction((gy-i), gx);
+					break;
+			}
 			// para de destruir ao encontrar parede
+			/*
 			if(map.cells[gy-i][gx].tipo === "paredeInd") {
 				destruir1 = false;
 			}
-			if(destruir1 && map.cells[gy-i][gx].tipo === "paredeDest") {
+			if(map.cells[gy-i][gx].tipo === "paredeDest") {
 				map.cells[gy-i][gx].tipo = "vazio";
 				destruir1 = false;
 			}
-			if(destruir1 && pc1.gx == gx && pc1.gy == gy-i) {
+			*/
+			if(pc1.gx == gx && pc1.gy == gy-i) {
 				atingiup1 = true;
 			}
-			if(destruir1 && pc2.gx == gx && pc2.gy == gy-i) {
+			if(pc2.gx == gx && pc2.gy == gy-i) {
 				atingiup2 = true;
 			}
 			if(destruir1) {
 				bomb.desenhaExplosao(ctx,bomb.x, (gy-i)*map.SIZE+map.SIZE/2);
 			}
 		}
-		if(gy+i < map.cells.length) {
+		if(gy+i < map.cells.length && destruir2) {
 			// para de destruir ao encontrar parede
 			if(map.cells[gy+i][gx].tipo === "paredeInd") {
 				destruir2 = false;
 			}
-			if(destruir2 && map.cells[gy+i][gx].tipo === "paredeDest") {
+			if(map.cells[gy+i][gx].tipo === "paredeDest") {
 				map.cells[gy+i][gx].tipo = "vazio";
 				destruir2 = false;
 			}
-			if(destruir2 && pc1.gx == gx && pc1.gy == gy+i) {
+			if(pc1.gx == gx && pc1.gy == gy+i) {
 				atingiup1 = true;
 			}
-			if(destruir2 && pc2.gx == gx && pc2.gy == gy+i) {
+			if(pc2.gx == gx && pc2.gy == gy+i) {
 				atingiup2 = true;
 			}
 			if(destruir2) {
 				bomb.desenhaExplosao(ctx,bomb.x, (gy+i)*map.SIZE+map.SIZE/2);
 			}
 		}
-		if(gx-i >= 0) {
+		if(gx-i >= 0 && destruir3) {
 			// para de destruir ao encontrar parede
 			if(map.cells[gy][gx-i].tipo === "paredeInd") {
 				destruir3 = false;
 			}
-			if(destruir3 && map.cells[gy][gx-i].tipo === "paredeDest") {
+			if(map.cells[gy][gx-i].tipo === "paredeDest") {
 				map.cells[gy][gx-i].tipo = "vazio";
 				destruir3 = false;
 			}
-			if(destruir3 && pc1.gx == gx-i && pc1.gy == gy) {
+			if(pc1.gx == gx-i && pc1.gy == gy) {
 				atingiup1 = true;
 			}
-			if(destruir3 && pc2.gx == gx-i && pc2.gy == gy) {
+			if(pc2.gx == gx-i && pc2.gy == gy) {
 				atingiup2 = true;
 			}
 			if(destruir3) {
 				bomb.desenhaExplosao(ctx,(gx-i)*map.SIZE+map.SIZE/2, bomb.y);
 			}
 		}
-		if(gx+i < map.cells[0].length) {
+		if(gx+i < map.cells[0].length && destruir4) {
 			// para de destruir ao encontrar parede
 			if(map.cells[gy][gx+i].tipo === "paredeInd") {
 				destruir4 = false;
 			}
-			if(destruir4 && map.cells[gy][gx+i].tipo === "paredeDest") {
+			if(map.cells[gy][gx+i].tipo === "paredeDest") {
 				map.cells[gy][gx+i].tipo = "vazio";
 				destruir4 = false;
 			}
-			if(destruir4 && pc1.gx == gx+i && pc1.gy == gy) {
+			if(pc1.gx == gx+i && pc1.gy == gy) {
 				atingiup1 = true;
 			}
-			if(destruir4 && pc2.gx == gx+i && pc2.gy == gy) {
+			if(pc2.gx == gx+i && pc2.gy == gy) {
 				atingiup2 = true;
 			}
 			if(destruir4) {
@@ -203,7 +215,45 @@ function explodir(bomb, map) {
 	}
 }
 
+function chainReaction(gy, gx) {
+	for(let i = pc1.bombs.length-1; i >= 0; i--) {
+		if(pc1.bombs[i].gx == gx && pc1.bombs[i].gy == gy) {
+			console.log("encontrou bomba");
+			explodir(pc1.bombs[i], map);
+			pc1.bombs.splice(i, 1);
+			return;
+		}
+	}
+	for(let i = pc2.bombs.length-1; i >= 0; i--) {
+		if(pc2.bombs[i].gx == gx && pc2.bombs[i].gy == gy) {
+			explodir(pc2.bombs[i], map);
+			pc2.bombs.splice(i, 1);
+			return;
+		}
+	}
+}
 
+function dropBomb(player, map) {
+	if(player.cooldown < 0 && player.bombs.length < player.maxBombs) {
+		var bomb = new Sprite();
+		var gx = Math.floor(player.x/map.SIZE);
+		var gy = Math.floor(player.y/map.SIZE);
+		// centraliza no grid
+		bomb.x = gx*map.SIZE + map.SIZE/2;
+		bomb.y = gy*map.SIZE + map.SIZE/2;
+		bomb.gx = gx;
+		bomb.gy = gy;
+		// nao deixa atravessar bombas !! faz personagem bugar e atravessar paredes !!
+		//map.cells[Math.floor(this.y/map.SIZE)][Math.floor(this.x/map.SIZE)] = 3;
+		bomb.w = 20;
+		bomb.h = 20;
+		bomb.timer = 2;
+		bomb.power = player.power;
+		map.cells[gy][gx].tipo = "bomba";
+		player.bombs.push(bomb);
+		player.cooldown = 0.2;
+	}
+}
 
 
 
@@ -225,25 +275,6 @@ function desenhaInfo(ctx) {
   }
 }
 
-function dropBomb(player, map) {
-	if(player.cooldown < 0 && player.bombs.length < player.maxBombs) {
-		var bomb = new Sprite();
-		var gx = Math.floor(player.x/map.SIZE);
-		var gy = Math.floor(player.y/map.SIZE);
-		// centraliza no grid
-		bomb.x = gx*map.SIZE + map.SIZE/2;
-		bomb.y = gy*map.SIZE + map.SIZE/2;
-		// nao deixa atravessar bombas !! faz personagem bugar e atravessar paredes !!
-		//map.cells[Math.floor(this.y/map.SIZE)][Math.floor(this.x/map.SIZE)] = 3;
-		bomb.w = 20;
-		bomb.h = 20;
-		bomb.timer = 2;
-		bomb.power = player.power;
-		map.cells[gy][gx].tipo = "bomba";
-		player.bombs.push(bomb);
-		player.cooldown = 0.2;
-	}
-}
 
 
 function initControls(){
